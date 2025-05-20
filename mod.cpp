7b66af86c1d728f83136ac076fd177deaa2315a2
tuntematonjr <mika.lieto@hotmail.com>
2025-05-20 21:44:19 +0300

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,27 @@
-name = "Modular FDF Pack";
-description = "FDF Reskins";
-picture = "FDF_Logo.paa";
+dir = "@fdf_modular_faction_pack";
+name = "FDF Modular Faction Pack v0.0.0";
 
-logo = "FDF_Logo.paa";
-logoOver = "FDF_Logo_Over.paa";
-
-tooltip = "Kartsa";
-tooltipOwned = "Kartsa";
-overview = "Modular FDF Pack";
 author = "Kartsa";
-overviewPicture = "FDF_Logo.paa";
+
+picture		 = "data\FDF_LOGO.paa";		   // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName		= "false";				  // Hide the extension name
+hidePicture	 = "false";				  // Hide the extension menu
+
+logoSmall	   = "data\FDF_LOGO.paa";		 // Display next to the item added by the mod 64x64
+logo			= "data\FDF_LOGO.paa";		// Logo displayed in the main menu 128x128
+logoOver		= "data\FDF_Logo_Over.paa"; // When the mouse is over, in the main menu 128x128
+tooltip		 = "FDF Reskins";
+tooltipOwned	= "FDF Reskins";  // Tool tip displayed when the mouse is left over, in the main menu
+overview		= "FDF Modular Faction Pack v0.0.0";		 // Overview text, displayed from the extension menu
+description	 = "It's unclear where this will show"; // Probably in context with action
+//action		  = "";	// Website URL, that can accessed from the expansions menu 
+//actionName	  = "Github";				 // label of button/tooltip in extension menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	1,
+	0.0,
+	0.86,
+	1
+};
