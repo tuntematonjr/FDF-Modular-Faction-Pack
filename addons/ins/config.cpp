64e7e542ef46c6a1b303a9c7e0187287c61d6fa5
tuntematonjr <mika.lieto@hotmail.com>
2025-05-21 21:50:50 +0300

    sort

--- addons/ins/config.cpp
+++ addons/ins/config.cpp
@@ -15,171 +15,171 @@ class CfgPatches {
 
 class CfgUnitInsignia {
 	class FDF_KAR_I_KRH {
-		displayName = "Mortar";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\KRH_INS_ca.paa";
+		displayName = "Mortar";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\KRH_INS_ca.paa";
 	};
     class FDF_KAR_I_HUO {
-		displayName = "Mechanic";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\HUO_INS_ca.paa";
+		displayName = "Mechanic";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\HUO_INS_ca.paa";
 	};
     class FDF_KAR_I_ART {
-		displayName = "Artillery Observer";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\ART_INS_ca.paa";
+		displayName = "Artillery Observer";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\ART_INS_ca.paa";
 	}; 
     class FDF_KAR_I_COA {
-		displayName = "Coastal Jaeger";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\COAST_INS_ca.paa";
+		displayName = "Coastal Jaeger";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\COAST_INS_ca.paa";
 	};
     class FDF_KAR_I_CRIMPB {
-		displayName = "MP Crisis Management (BROWN)";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\CRIMPB_INS_ca.paa";
+		displayName = "MP Crisis Management (BROWN)";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\CRIMPB_INS_ca.paa";
 	}; 
     class FDF_KAR_I_CRIMP {
-		displayName = "MP Crisis Management (BLACK)";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\CRIMP_INS_ca.paa";
+		displayName = "MP Crisis Management (BLACK)";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\CRIMP_INS_ca.paa";
 	};
     class FDF_KAR_I_EOD {
-		displayName = "EOD";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\EOD_INS_ca.paa";
+		displayName = "EOD";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\EOD_INS_ca.paa";
 	};
     class FDF_KAR_I_IEDD {
-		displayName = "IEDD";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\IEDD_INS_ca.paa";
+		displayName = "IEDD";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\IEDD_INS_ca.paa";
 	};
     class FDF_KAR_I_FL {
-		displayName = "Insignia Patch";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\FL_INS_ca.paa";
+		displayName = "Insignia Patch";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\FL_INS_ca.paa";
 	};
     class FDF_KAR_I_JAEGER {
-		displayName = "Jaeger";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\JAA_INS_ca.paa";
+		displayName = "Jaeger";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\JAA_INS_ca.paa";
 	}; 
     class FDF_KAR_I_MEDIC {
-		displayName = "Medic";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\MED_INS_ca.paa";
+		displayName = "Medic";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\MED_INS_ca.paa";
 	};
     class FDF_KAR_I_SISSI {
-		displayName = "Sissi";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\SISSI_INS_ca.paa";
+		displayName = "Sissi";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\SISSI_INS_ca.paa";
 	};
     class FDF_KAR_I_PIONEER {
-		displayName = "Pioneer";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\PIO_INS_ca.paa";
+		displayName = "Pioneer";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\PIO_INS_ca.paa";
 	};
 	class FDF_KAR_I_VIESTI {
-		displayName = "Signals";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\VIE_INS_CA.paa";
+		displayName = "Signals";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\VIE_INS_CA.paa";
 	};
 	class FDF_KAR_I_NBC {
-		displayName = "NBC";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\NBC_INS_CA.paa";
+		displayName = "NBC";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\NBC_INS_CA.paa";
 	};
 	class FDF_KAR_I_TYK {
-		displayName = "Artillery";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\TYK_INS_CA.paa";
+		displayName = "Artillery";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\TYK_INS_CA.paa";
 	};
 	class FDF_KAR_I_PST {
-		displayName = "Anti-Tank";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\PST_INS_CA.paa";
+		displayName = "Anti-Tank";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\PST_INS_CA.paa";
 	};
 	class FDF_KAR_I_PST_J {
-		displayName = "AT Jaeger";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\PSTJ_INS_CA.paa";
+		displayName = "AT Jaeger";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\PSTJ_INS_CA.paa";
 	};
 	class FDF_KAR_I_PSVM {
-		displayName = "Tanker";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\PSVM_INS_CA.paa";
+		displayName = "Tanker";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\PSVM_INS_CA.paa";
 	};
 	class FDF_KAR_I_VENE {
-		displayName = "Boatman";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\VENE_INS_CA.paa";
+		displayName = "Boatman";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\VENE_INS_CA.paa";
 	};
 	class FDF_KAR_I_TUTKA {
-		displayName = "Radarman";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\TUTKA_INS_CA.paa";
+		displayName = "Radarman";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\TUTKA_INS_CA.paa";
 	};
 	class FDF_KAR_I_DOG {
-		displayName = "Dog Handler";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\DOG_INS_CA.paa";
+		displayName = "Dog Handler";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\DOG_INS_CA.paa";
 	};
 	class FDF_KAR_I_SPOL {
-		displayName = "Military Police";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\SPOL_INS_CA.paa";
+		displayName = "Military Police";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\SPOL_INS_CA.paa";
 	};
 	class FDF_KAR_I_AAOHJ {
-		displayName = "AA-Missile";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\AAOHJ_INS_CA.paa";
+		displayName = "AA-Missile";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\AAOHJ_INS_CA.paa";
 	};
 	class FDF_KAR_I_RPIO {
-		displayName = "EOD Pioneer";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\RPIO_INS_CA.paa";
+		displayName = "EOD Pioneer";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\RPIO_INS_CA.paa";
 	};
 	class FDF_KAR_I_AAM {
-		displayName = "Anti-Air";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\AAM_INS_CA.paa";
+		displayName = "Anti-Air";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\AAM_INS_CA.paa";
 	};
 	class FDF_KAR_I_FLAG {
-		displayName = "Finnish Flag";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\FLAG_INS_CA.paa";
+		displayName = "Finnish Flag";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\FLAG_INS_CA.paa";
 	};
 	class FDF_KAR_I_SP {
-		displayName = "Military Police (Patch)";
 		author = "Kartsa";
-		texture = "\KAR_FDF_INS\data\SP_INS_CA.paa";
+		displayName = "Military Police (Patch)";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		texture = "\KAR_FDF_INS\data\SP_INS_CA.paa";
 	};
 };
